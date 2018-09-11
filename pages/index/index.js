b85d6ee631da0ea742d47eda614844e0cadfc3fc
we1zheng <we1zheng@qq.com>
2018-09-11 10:25:31 +0800

    提交文件

---
+++ pages/index/index.js
@@ -0,0 +1,88 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var WxSearch = require('../../wxSearchView/wxSearchView.js');
+
+
+Page({
+  data: {
+    motto: '淄博市公章刻制需求材料查询',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    // 2 搜索栏初始化
+    var that = this;
+    WxSearch.init(
+      that,  // 本页面一个引用
+      ['公章', '财务章', "发票章", "法人章"], // 热点搜索推荐，[]表示不使用
+      ['', '', '', ""],// 搜索匹配，[]表示不使用
+      that.mySearchFunction, // 提供一个搜索回调函数
+      that.myGobackFunction //提供一个返回回调函数
+    );
+
+  },
+  // 3 转发函数，固定部分，直接拷贝即可
+  wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
+  wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
+  wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
+  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+  wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
+  // 4 搜索回调函数  
+  mySearchFunction: function (value) {
+    // do your job here
+    // 示例：跳转
+    wx.redirectTo({
+      url: '../index/index?searchValue=' + value
+    })
+  },
+  // 5 返回回调函数
+  myGobackFunction: function () {
+    // do your job here
+    // 示例：返回
+    wx.redirectTo({
+      url: '../index/index?searchValue=返回'
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
